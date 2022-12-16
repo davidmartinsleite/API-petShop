d2af10d28a2662c1b7f2487280dadfabf293fcc9
David Martins Leite <davidmartinsleite@hotmail.com>
2022-12-16 19:03:28 -0300

    CRUD da API basica concluida

--- fastAPI.py
+++ fastAPI.py
@@ -36,3 +36,31 @@ def atualizar_tutor(id: int, nome: str, endereco: str, telefone: str):
     tutor_atualizado = RepositorioTutor().atualizar(id=id, nome=nome, endereco=endereco, telefone=telefone)
     return tutor_atualizado
 
+
+@app.get('/pets/seleciona')  # criar um endpoint para verificar todos os objetos criados
+def pega_todos_os_pets():
+    return RepositorioPet().selecionar()
+
+
+@app.get('/pets/seleciona/{id}')
+def pega_pet_especifico(id: int):
+    return RepositorioPet().selecionar_especifico(id)
+
+
+@app.post('/pets/adcionar')
+def adiona_novo_pet(nome_pet: str, idade: int, peso: float, tutor_id: int):
+    pet = Pet(nome_pet=nome_pet, idade=idade, peso=peso, tutor_id=tutor_id)
+    pet_adicionado = RepositorioPet().adicionar(pet)
+    return pet_adicionado
+
+
+@app.delete('/pets/deletar/{id}')
+def deletar_pet(id: int):
+    confirmacao = RepositorioPet().deletar(id)
+    return confirmacao
+
+
+@app.patch('/pets/atualizar')
+def atualizar_pet(id: int, nome_pet: str, idade: int, peso: float, tutor_id: int):
+    pet_atualizado = RepositorioPet().atualizar(id=id, nome_pet=nome_pet, idade=idade, peso=peso, tutor_id=tutor_id)
+    return pet_atualizado
